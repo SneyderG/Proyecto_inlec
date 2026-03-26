758f3e5630b62f8fcf9a6484b1cad6aa436a601e
sneyder <sneyder.gaviria180107@gmail.com>
2026-03-26 10:49:57 -0500

    version 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "../stylesheets/Header.css"
 
 
 export const Header = () => {
@@ -7,53 +8,33 @@ export const Header = () => {
         <>
         
            
-            <nav className="navbar navbar-expand-lg" >
-                <div className="container">
+            <nav className="navbar navbar-expand-lg bg-wave" >
+                <div className="container gap-3" >
+                    <img height="50" src="./loi.png" alt="" />
 
-                    <Link className="navbar-brand" to="/" style={{ color: "#000000", fontFamily: "'carton'", }}>Home</Link>
+                    <Link className="navbar-brand" to="/" style={{ color: "#000000", fontFamily: "'carton'", }}>Inicio</Link>
 
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ color: "#000000", fontFamily: "'carton'", }}>
-                                    Browse
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#" >Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ color: "#000000", fontFamily: "'carton'", }}>
-                                    Kids
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
+                            <li className="nav-item">
+                                <Link className='nav-link' aria-current="page" to="/misterio"  style={{ color: "#000000", fontFamily: "'carton'", }}>Sobre nosotros</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className='nav-link' aria-current="page" to="/misterio"  style={{ color: "#000000", fontFamily: "'carton'", }}>Terror</Link>
+                                <Link className='nav-link' to="/Terror" style={{ color: "#000000", fontFamily: "'carton'", }} >Capitulos</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className='nav-link' to="/Terror" style={{ color: "#000000", fontFamily: "'carton'", }} >Misterio</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className='nav-link' to="/Suspenso" style={{ color: "#000000", fontFamily: "'carton'", }} >Suspenso</Link>
+                                <Link className='nav-link' to="/Suspenso" style={{ color: "#000000", fontFamily: "'carton'", }} >Mas informacion</Link>
                             </li>
 
                         </ul>
                         <div>
                             <form className="d-flex" role="search">
                                 <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                                <button className="btn btn-outline-success" type="submit">🔔</button>
+                               
                             </form>
                         </div>
                     </div>
